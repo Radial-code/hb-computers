71dc3e75258372cce7363a8608dcd7c1cb256779
Neha Rai <neharadialcode@gmail.com>
2023-08-24 10:26:34 +0530

    console error removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,26 +66,8 @@ function shownav() {
   document.body.classList.toggle("overflow-hidden");
 }
 
-// Get the button
-let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 // service dropdown
 const dropdownToggle = document.querySelector(".dropdown-toggle");
 const dropdownContent = document.querySelector(".dropdown-content");
